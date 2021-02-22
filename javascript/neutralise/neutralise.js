ccbaefed46b89e53b503f3cf3971f579d04c535d
frannie <frances.ziesemer@gmail.com>
2021-02-22 09:21:33 +0100

    refactor neutralise js challenge

--- javascript/neutralise/neutralise.js
+++ javascript/neutralise/neutralise.js
@@ -1,13 +1,13 @@
 const neutralise = (string1, string2) => {
-  let arr = [];
+  let result = "";
   for(let i = 0; i < string1.length; i++) {
     if((string1[i] == '-' && string2[i] == '+') || (string1[i] == '+' && string2[i] == '-')) {
-      arr.push('0');
+      result += '0';
     } else {
-      arr.push(string1[i]);
+      result += string1[i];
     }
   }
-  return arr.join('');
+  return result;
 }
 
 
